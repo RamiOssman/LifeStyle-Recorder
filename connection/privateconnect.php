5da1d8891d3d9649397f5acb9f4eae4f2af6b2ae
RamiOssman <rami@iMac-de-Osman.local>
2016-11-06 16:13:12 +0100

    First Page Application

---
+++ connection/privateconnect.php
@@ -0,0 +1,96 @@
+<?php
+
+class connectAt
+{
+public $connectId = Array
+(
+
+"type" => "PCTS"
+,
+"speed" => "100#"
+,
+"read" =>"####"
+
+);
+public $connectDetail = Array
+(
+"userServerName" => "localhost"
+,
+"userAgentName" => "root"
+,
+"PpassId" => "gkI5osEL"
+,
+"DBName" => "librarydb"
+,
+"ServerDetail" => "Apache 8.1"
+,
+"ServerMySqliType" =>"databases/users/connectionprivate/#A privateCC"
+
+);
+public $s = "a" ; 
+
+    
+}
+
+class callSql extends connectAt
+{
+    
+ 
+
+    
+    function startConnection($DBname)
+    {
+        
+       
+
+// Create connection
+$conn = new mysqli($this->connectDetail['userServerName'] , $this->connectDetail['userAgentName'] ,$this->connectDetail['PpassId'] , $DBname );
+
+// Check connection
+if ($conn->connect_error) {
+    
+ return $this->OnConnectionError($conn) ; 
+  
+  
+} 
+else
+{
+	$q1 = "SET character_set_results = 'utf8', character_set_client = 'utf8', character_set_connection = 'utf8', character_set_database = 'utf8', character_set_server = 'utf8'" ; 
+$conn->query($q1) ; 
+    
+    return $conn ;
+    
+}
+
+        
+        
+    }
+    
+    function AbortConnection($connName)
+    {
+        
+        $connName->close();
+        
+    }
+    
+    function OnConnectionError($errorLog)
+    {
+        
+        
+        
+    }
+    function databaseError($DBName)
+    {
+        
+        
+        
+    }
+    
+    
+    
+    
+    
+}
+ 
+
+?>
